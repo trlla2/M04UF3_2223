a2427f79330817e80da72cdcae0e3a3148eecb83
trlla2 <roc.allue@enti.cat>
2023-02-08 08:49:00 +0100

    adsf

--- indexs.js
+++ indexs.js
@@ -6,8 +6,22 @@ const fs = require("fs");
 
 http.createServer(function(request, response){
 
+
+	if (request.url == '/player.jpeg'){
+		fs.readFile("img/player.jpeg", function (err, data){
+			if(err){
+				console.log(err);
+				return;
+			}
+			response.writeHead(200, {"Content-Type":"image/jpeg"});
+			response.write(data);
+		});
+	}
+	
+
 	fs.readFile("indexs.html", function(err, data){
-		if(err){
+	
+	if(err){
 			console.error(err);
 			return;
 		}
